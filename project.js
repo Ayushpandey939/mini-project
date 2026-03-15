2d2979593f81778b4d8a3849010b796c48549851
Ayush Pandey <ayushpandey9482@gmail.com>
2026-03-16 01:29:23 +0530

    Login page code

---
+++ project.js
@@ -0,0 +1,12 @@
+    function togglePassword(){
+
+    let pass = document.getElementById("password");
+
+    if(pass.type === "password"){
+        pass.type = "text";
+    }
+    else{
+        pass.type = "password";
+    }
+
+}
